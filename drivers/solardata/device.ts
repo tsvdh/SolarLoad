80c0b0b4e5cb87d8d99614f76bd686032085369e
Tobias van den Hurk <tsvdhurk@gmail.com>
2023-06-27 17:04:09 +0200

    add averages

--- drivers/solardata/device.ts
+++ drivers/solardata/device.ts
@@ -19,7 +19,11 @@ class SolarDevice extends Homey.Device {
     dataURL = 'https://www.weerzoetermeer.nl/clientraw/clientraw.txt';
     values: number[] = [];
 
+    timeFrames = [5, 10, 15, 30, 60];
+
     getAverageValue(duration: number) {
+        duration = Math.min(duration, this.values.length);
+
         const wantedValues = this.values.slice(0, duration);
 
         let average = wantedValues.reduce((accumulator, current) => {
@@ -35,30 +39,57 @@ class SolarDevice extends Homey.Device {
      * onInit is called when the device is initialized.
      */
     async onInit() {
+        const options = {
+            decimals: 0,
+            units: 'W/m\u00B2',
+        };
+
+        await this.setCapabilityOptions('measure_luminance', {
+            ...options,
+            title: {
+                en: 'Luminance now',
+                nl: 'Helderheid nu',
+            },
+        });
+
+        for (const timeFrame of this.timeFrames) {
+            const id = `measure_luminance.${timeFrame}min`;
+            await this.addCapability(id);
+
+            await this.setCapabilityOptions(id, {
+                ...options,
+                title: {
+                    en: `Luminance ${timeFrame} min`,
+                    nl: `Helderheid ${timeFrame} min`,
+                },
+            });
+        }
+
+        this.log('Added capabilities');
+
         const getData = async () => {
             const rawData = await axios.get<string>(this.dataURL).then((res) => res.data);
 
             const newValue = parseFloat(rawData.split(' ')[127]);
 
-            await this.setCapabilityValue('measure_luminance', newValue);
-
             this.values.unshift(newValue);
             if (this.values.length > 60) {
                 this.values.pop();
+            }
+
+            await this.setCapabilityValue('measure_luminance', newValue);
+
+            for (const timeFrame of this.timeFrames) {
+                if (timeFrame <= this.values.length) {
+                    await this.setCapabilityValue(`measure_luminance.${timeFrame}min`, this.getAverageValue(timeFrame));
+                }
             }
         };
 
         await getData();
         this.homey.setInterval(getData, 1000 * 60);
 
-        this.log('Capturing data flow...');
-
-        const options = {
-            decimals: 0,
-            units: 'W/m\u00B2',
-        };
-
-        await this.setCapabilityOptions('measure_luminance', options);
+        this.log('Connected to data flow...');
 
         const sunConditionMore = this.homey.flow.getConditionCard('sun_more_less');
         sunConditionMore.registerRunListener((args: SunConditionMore, state) => {
